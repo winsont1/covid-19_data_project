2544eb35eb36a36ecc7136123b6002829da65c5c
Winson Tan <winsont89@gmail.com>
2020-04-23 15:51:49 +1000

    First commit

---
+++ deaths_change_calculation.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE deaths_change_sql AS
+	SELECT
+	country,
+	date,
+	total_deaths - lag(total_deaths) OVER (PARTITION BY country ORDER BY date ASC) as deaths_change
+	FROM deaths_total;
+
+UPDATE
+    deaths_change_sql
+SET
+    deaths_change = 0
+WHERE
+    deaths_change IS NULL;
